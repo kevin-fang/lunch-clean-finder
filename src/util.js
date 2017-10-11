2a16486c5e021b1bbae4ee917127cd83c3aa3aea
Kevin Fang <kfang1233@gmail.com>
2017-10-10 22:15:37 -0400

    Fixed last names with space not working

--- src/util.js
+++ src/util.js
@@ -1,6 +1,12 @@
 String.prototype.nameify = function() {
     if (this.includes('-')) { // capitalize text after hyphen if needed
-        var idx = this.indexOf('-') + 1 // get text after hyphen
+        let idx = this.indexOf('-') + 1 // get text after hyphen
+        // capitalize first letter, lower case until the hyphen, and then recursively nameify that
+        return this.charAt(0).toUpperCase()
+            + this.substring(1, idx).toLowerCase()
+            + this.substring(idx).nameify()
+    } else if (this.includes(' ')) {
+        let idx = this.indexOf(' ') + 1 // get text after hyphen
         // capitalize first letter, lower case until the hyphen, and then recursively nameify that
         return this.charAt(0).toUpperCase()
             + this.substring(1, idx).toLowerCase()
@@ -12,7 +18,7 @@ String.prototype.nameify = function() {
 
 Date.prototype.isToday = function() {
 	this.setHours(0, 0, 0, 0)
-	var today = new Date()
+	let today = new Date()
 	today.setHours(0, 0, 0, 0)
 	return this.valueOf() === today.valueOf()
 }
